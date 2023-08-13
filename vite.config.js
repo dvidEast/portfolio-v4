14383a956607aac1ec04eb88ba367cee75cfe41c
David Lim <daviddlim11@gmail.com>
2023-08-13 15:20:15 -0700

    react-router fix

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,10 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+
+  resolve: {
+    alias: {
+      'react-router-dom': 'react-router-dom', // Adjust this path as needed
+    },
+  },
 })
