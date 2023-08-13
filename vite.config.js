d0256a76aeba1989f67143ecf914ed89eebf30d5
David Lim <daviddlim11@gmail.com>
2023-08-13 13:02:26 -0700

    second vite config

--- vite.config.js
+++ vite.config.js
@@ -3,7 +3,7 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/portfolio-v4',
-  
+  base: '/portfolio-v4/',
+
   plugins: [react()],
 })
